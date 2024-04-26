c1fbccc40148a995ee0d039be7238fa3d969a295
Echo-jyt <jyt313412@163.com>
2024-04-26 21:38:34 +0800

    第二天学东西 1080ti

---
+++ 第1章 开始/1.5/ConsoleApplication1/Version_test.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifdef lround
+inline long lround(double d)
+{
+    return (d >= 0) ? long(d + 0.5) : long(d - 0.5);
+}
+#endif
+
--- 第1章 开始/1.5/ConsoleApplication1/main.cpp
+++ 第1章 开始/1.5/ConsoleApplication1/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include<iostream>
 
-#include"Point.h"
+
 using namespace std;
 
 int main()
